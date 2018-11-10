ec36d6e2bc5176671446460cda610b84f51f1454
Priyanshi Nigam <priyanshi.nigam@gmail.com>
2018-11-09 19:47:33 -0800

    initial commit

---
+++ Assignment_1/src/Foothill.java
@@ -0,0 +1,24 @@
+
+public class Foothill {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Priyanshi Nigam.");
+		System.out.println("My Foothill ID # is 20367874");
+		System.out.println(("Syllabus detail #1: Put CS1A in subject when emailing the professor about a private question. \n") +
+				("Syllabus detail #2: Comments are worth 20% of the assignment grades. \n") + 
+				("Syllabus detail #3: No late assignments will be accepted after Wednesday, August 8. \n") + 
+				("Syllabus detail #4: Textbook, notes, past assignments, and compiling your answers are allowed during the final. \n") + 
+				("Syllabus detail #5: Final grades are composed of assignments, discussions, midterm, and the final."));
+	}
+}
+
+/* ---------- paste of run from console window------------------
+Hello World!
+My name is Priyanshi Nigam.
+My Foothill ID # is 20367874
+Syllabus detail #1: Put CS1A in subject when emailing the professor about a private question. 
+Syllabus detail #2: Comments are worth 20% of the assignment grades. 
+Syllabus detail #3: No late assignments will be accepted after Wednesday, August 8. 
+Syllabus detail #4: Textbook, notes, past assignments, and compiling your answers are allowed during the final. 
+Syllabus detail #5: Final grades are composed of assignments, discussions, midterm, and the final.
+ ----------------------------------------------------------------*/
